39ddb21b6225ca4e407a41a60d17f7c4beb42a01
visscher <visscher@zorgdomein.nl>
2019-12-16 15:21:59 +0100

    Rewrite reporterconfig to add timestamps and make filenames with timestamp in common directory.

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/kiwivogel/ecr-scan-util/reporters"
 	"gopkg.in/alecthomas/kingpin.v2"
 	"strings"
+	"time"
 )
 
 var (
@@ -16,6 +17,7 @@ var (
 	baseRepo       = kingpin.Flag("baserepo", "Common prefix for images. E.g. zorgdomein").Envar("ESA_ECR_BASE_REPO").Default("zorgdomein").String()
 	containerName  = kingpin.Flag("container", "Container name to fetch scan results for").Envar("ESA_ECR_CONTAINER_NAME").Default("nexus").String()
 	containerTag   = kingpin.Flag("tag", "Container tag or hash to fetch scan results for").Envar("ESA_ECR_CONTAINER_IDENTIFIER").Default("2.14.12-02-30102019").String()
+	reportDir      = kingpin.Flag("directory", "Directory to write reports to").Envar("ESA_REPORT_DIR").Default("reports").String()
 	severityCutoff = kingpin.Flag("cutoff", "Severity to count as failures").Envar("ESA_SEVERITY_CUTOFF").Default("MEDIUM").String()
 	//TODO: Implement hash based findings, Probably requires further abstraction of *ecrDescribeImageScanFindingsInput
 	//containerHash =  kingpin.Flag("hash", "Container hash to fetch scan results for").Envar("ESA_ECR_CONTAINER_HASH").String()
@@ -24,14 +26,11 @@ var (
 	//reporterConfigFile = kingpin.Flag("reporter", "Configuration file for configuring reporters").Envar("ESA_REPORTER_CONFIG").Default("").String()
 )
 
-///TODO: ADD MORE MESSAGES TO CHECK INVOCATIONS. This doesn't really help like this.
-
 func main() {
 
 	kingpin.Parse()
 
 	findings := map[string]ecr.ImageScanFindings{}
-	ReporterConfig := helpers.NewDefaultReporterConfig()
 
 	if *composition != "" {
 
@@ -45,7 +44,7 @@ func main() {
 
 		for f := range findings {
 
-			singleReporterConfig := helpers.NewCustomReporterConfig("report.xml", fmt.Sprintf("%s/", f), *reporterList)
+			singleReporterConfig := helpers.NewCustomReporterConfig(fmt.Sprintf("%s-%s.xml", f, time.Now().Format(time.RFC850)), fmt.Sprintf("%s/", *reportDir), *reporterList)
 			if singleReporterConfig.ReporterType == "junit" {
 				se := reporters.CreateXmlReport(f, *severityCutoff, findings[f], singleReporterConfig)
 				helpers.Check(se, fmt.Sprintf("Failed to write report for %s", f))
@@ -56,6 +55,7 @@ func main() {
 	} else {
 
 		repositoryName := strings.Join([]string{*baseRepo, *containerName}, "/")
+		ReporterConfig := helpers.NewCustomReporterConfig(fmt.Sprintf("%s-%s.xml", *containerName, time.Now().Format(time.RFC850)), fmt.Sprintf("%s/", *reportDir), *reporterList)
 
 		result, err := aggregator.EcrGetScanResultsByTag(repositoryName, *containerTag, *registryId)
 		helpers.Check(err, fmt.Sprintf("Failed to write report for %s:%s", *containerName, *containerTag))
